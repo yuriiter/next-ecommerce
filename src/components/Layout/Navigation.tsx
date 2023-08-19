d62d53b87d0ac5fc005007c856e56fe175ae3999
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-08-19 22:36:04 +0200

    Layout component

---
+++ src/components/Layout/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Logo } from "../Logo";
+
+export const Navigation = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+import { Navigation } from "./Navigation";
+import { Footer } from "./Footer";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <Navigation />
+      {children}
+      <Footer />
+    </div>
+  );
+};
