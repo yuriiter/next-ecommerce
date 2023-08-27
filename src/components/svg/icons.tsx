77ebce0271fff4c4066cb9d70fa153d3c695b0f0
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-08-27 21:31:12 +0200

    add folder for svg - icons and backgrounds

--- src/components/svg/icons.tsx
+++ src/components/svg/icons.tsx
@@ -1,6 +1,5 @@
 import { SVGProps } from "react";
-
-type IconProps = SVGProps<SVGSVGElement>;
+import { IconProps } from "./types";
 
 export const TurnOnWifiIcon = (props: IconProps) => {
   return (
