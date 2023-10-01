8df05b725c7defe0b211ef37abf07a09beb7ad21
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-10-01 21:12:32 +0200

    navigation position: fixed and hide or show it on scroll

--- src/components/Layout/Navigation/Navigation.tsx
+++ src/components/Layout/Navigation/Navigation.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Logo } from "../../Logo";
 import { Search } from "./Search";
 import { NavigationButtons } from "./NavigationButtons/NavigationButtons";
+import { useHideNavigation } from "./useHideNavigation";
 
 export const Navigation = () => {
+  const hide = useHideNavigation();
+
   return (
-    <div className="navigation">
-      <div className={`container navigation__container`}>
-        <Logo />
-        <Search />
-        <NavigationButtons />
+    <>
+      <div className={`navigation ${hide ? "navigation--hidden" : ""}`}>
+        <div className={`container navigation__container`}>
+          <Logo />
+          <Search />
+          <NavigationButtons />
+        </div>
       </div>
-    </div>
+      <div className="navigation__placeholder"></div>
+    </>
   );
 };
---
+++ src/components/Layout/Navigation/useHideNavigation.ts
@@ -0,0 +1,24 @@
+import React, { useCallback, useEffect, useState, useRef } from "react";
+
+export const useHideNavigation = () => {
+  const [hide, setHide] = useState(false);
+  const prevScrollTopRef = useRef<number>(0);
+
+  useEffect(() => {
+    prevScrollTopRef.current = window.scrollY;
+  }, []);
+
+  const onScroll = useCallback(() => {
+    const { scrollY } = window;
+    setHide(scrollY > prevScrollTopRef.current && scrollY > 132);
+    prevScrollTopRef.current = scrollY;
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return hide;
+};
