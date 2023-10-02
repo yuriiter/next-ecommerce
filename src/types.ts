c456ef5e1b3b6cf824b30584ea79ee86709fed86
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-10-03 00:46:05 +0200

    extend car data type

--- src/types.ts
+++ src/types.ts
@@ -2,17 +2,33 @@ import { ReactNode } from "react";
 
 type CarType = "SUV" | "sport" | "hatchback" | "sedan";
 
+export type Review = {
+  avatar: string;
+  fullName: string;
+  caption: string;
+  date: Date;
+  rating: number;
+  comment: string;
+};
+
 export type CarData = {
   id: string;
   name: string;
+  title?: string;
+  subTitle?: string;
   carType: CarType;
   fuelCapacity: number;
   peopleCapacity: number;
   isManual: boolean;
   price: number;
   previousPrice?: number;
-  isInFavorites: boolean;
+  isInFavorites?: boolean;
   thumbnailURL: string;
+  photosURLs?: string[];
+  description: string;
+  rating?: number;
+  numOfVotes?: number;
+  reviews?: Review[];
 };
 
 export type LinkData = {
