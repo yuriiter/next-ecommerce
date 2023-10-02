8c61db9d16a351f93b7a1c8dafb42f3928471260
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-10-03 00:45:54 +0200

    clamp function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 export const mod = (a: number, n: number): number => {
   return a - n * Math.floor(a / n);
 };
+
+export const clamp = (min: number, max: number, value: number) =>
+  Math.min(Math.max(value, min), max);
