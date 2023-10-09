b2986f4ad7819ebd4a74c6aa77511c92d8857e57
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2023-10-09 22:35:18 +0200

    rating component

---
+++ src/components/CarDetail/Rating.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { StarEmpty, StarYellow } from "../svg/icons";
+
+type RatingProps = {
+  rating: number;
+  numOfVotes: number;
+  className?: string;
+  onVote?: (rating: number) => void | Promise<void>;
+};
+
+export const Rating = ({
+  rating,
+  numOfVotes,
+  className = "",
+  onVote,
+}: RatingProps) => {
+  const yellowStarsLen = Math.round(rating);
+  const emptyStarsLen = 5 - yellowStarsLen;
+
+  const yellowStars = Array.from({ length: yellowStarsLen }, (value, index) => {
+    const onClick = () => onVote?.(index);
+    return <StarYellow key={index} onClick={onClick} />;
+  });
+
+  const emptyStars = Array.from({ length: emptyStarsLen }, (value, index) => {
+    const onClick = () => onVote?.(index);
+    return <StarEmpty key={index} onClick={onClick} />;
+  });
+
+  return (
+    <div className="rating__wrapper">
+      <div className={`rating ${className}`}>
+        {yellowStars}
+        {emptyStars}
+      </div>
+      <span className="rating__num-of-votes">{numOfVotes}+ reviews</span>
+    </div>
+  );
+};
