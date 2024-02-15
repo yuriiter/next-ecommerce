9b45cb3afe8d9968173d01ba6a88b8cba5c1212f
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-02-15 22:33:49 +0100

    change structure for sidebar inputs

--- src/components/Sidebar/types.ts
+++ src/components/Sidebar/types.ts
@@ -2,17 +2,8 @@ export type SidebarInputType = "switch" | "range";
 
 export type SidebarInput = {
   name: string;
-  inputType: SidebarInputType;
-  numOfItemsOfType?: number;
-} & (
-  | {
-      inputType: "switch";
-      value: boolean;
-    }
-  | {
-      inputType: "range";
-      value: number;
-    }
-);
+  value: boolean | number;
+  key: string;
+};
 
-export type SidebarInputs = Record<string, SidebarInput[]>;
+export type SidebarInputGroup = { groupName: string; inputs: SidebarInput[] };
