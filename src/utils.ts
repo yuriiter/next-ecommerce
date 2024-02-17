7673b4e51bdbf739270502b7fb508cce054b1038
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-02-17 02:17:37 -0500

    uuid utility

--- src/utils.ts
+++ src/utils.ts
@@ -18,3 +18,13 @@ export const cn = (classNames: (string | undefined | null | boolean)[]) =>
     .join(" ");
 
 export const copy = <T>(ob: T) => JSON.parse(JSON.stringify(ob)) as T;
+
+export const uuid = () => {
+  let dt = new Date().getTime();
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+};
