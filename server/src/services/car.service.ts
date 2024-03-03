eca6aa2419cda0f7fb60e92488799c61c4dfbcf3
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-03 00:59:55 -0800

    get cars logic

--- server/src/services/car.service.ts
+++ server/src/services/car.service.ts
@@ -1,1 +1,15 @@
-export const getCarCards = (userId: string | undefined, filters) => {}
+import { CarModel } from "@models/index"
+import { type carsQuery } from "@typings/carsQuery"
+import { buildCarQuery } from "@utils/buildCarsQuery"
+
+export const getCars = async (query: carsQuery, userEmail?: string) => {
+    const finalQuery = buildCarQuery(query)
+
+    const { page = 0, pageSize = 8 } = query
+
+    const cars = await CarModel.find(finalQuery)
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+
+    return cars
+}
