e9ad1766328dc9daaef0963ce139da493c564d65
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-04 22:05:32 -0800

    use old way of transforming sidebar form

--- src/hooks/URLQueries/useFilters.ts
+++ src/hooks/URLQueries/useFilters.ts
@@ -5,12 +5,10 @@ import { useURLQueryObjectState } from "./useURLQueryObjectState";
 import { copy } from "@/utils";
 
 const sidebarInputsToQueryState = (sidebarInputs: SidebarInputGroup[]) =>
-  sidebarInputs.reduce((filtersObject, { groupName, inputs }) => {
-    const inputsString = inputs
-      .filter(({ value }) => ![null, undefined, false, ""].includes(value))
-      .join(",");
-
-    if (inputsString !== "") filtersObject[groupName] = inputsString;
+  sidebarInputs.reduce((filtersObject, group) => {
+    group.inputs.forEach(({ key, value }) => {
+      filtersObject[key] = value;
+    });
     return filtersObject;
   }, {});
 
