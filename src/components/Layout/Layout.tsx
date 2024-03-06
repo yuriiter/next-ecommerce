82c936ff124030f0f948bc32ba9a788d52617b11
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-05 16:57:10 -0800

    add sidebar, modal window and auth contexts to app

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,9 @@ import { Navigation } from "./Navigation";
 import { Footer } from "./Footer";
 import { ToastContainer } from "../Toast/ToastContainer";
 import { ToastContextProvider } from "../Toast/ToastContext";
+import { SidebarContextProvider } from "../Sidebar/SidebarContext";
+import { ModalWindowContextProvider } from "../ModalWindow/ModalWindowContext";
+import { AuthContextProvider } from "@/auth/AuthContext";
 
 type Props = {
   children: ReactNode;
@@ -10,15 +13,23 @@ type Props = {
 
 export const Layout = ({ children }: Props) => {
   return (
-    <ToastContextProvider>
-      <div className="content">
-        <Navigation />
-        <div className="width-sidebar">
-          <div className="width-sidebar__content">{children}</div>
-        </div>
-        <Footer />
-      </div>
-      <ToastContainer />
-    </ToastContextProvider>
+    <>
+      <AuthContextProvider>
+        <ModalWindowContextProvider>
+          <SidebarContextProvider>
+            <ToastContextProvider>
+              <div className="content">
+                <Navigation />
+                <div className="width-sidebar">
+                  <div className="width-sidebar__content">{children}</div>
+                </div>
+                <Footer />
+              </div>
+              <ToastContainer />
+            </ToastContextProvider>
+          </SidebarContextProvider>
+        </ModalWindowContextProvider>
+      </AuthContextProvider>
+    </>
   );
 };
