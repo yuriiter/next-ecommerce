f515f8e8dca09e4166274444c61c721ec915b322
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-05 16:47:50 -0800

    set defaults for axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/main.scss";
+import axios from "axios";
 import type { AppProps } from "next/app";
+
+axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
