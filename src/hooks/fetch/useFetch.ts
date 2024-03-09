944a925e0df7636a901776e842c50cb5845e344c
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-09 13:27:28 -0800

    enhance useFetch hook

--- src/hooks/fetch/useFetch.ts
+++ src/hooks/fetch/useFetch.ts
@@ -8,13 +8,13 @@ import {
 import { useCallback, useEffect, useRef, useState } from "react";
 import axios, { CancelTokenSource } from "axios";
 
-export const useFetch = <T, D = any>({
+export const useFetch = <T>({
   url,
   requestConfig,
   pause,
-}: UseFetchParams<D>): [FetchStatus<T>, FetchCallback<T>] => {
+}: UseFetchParams): [FetchStatus<T>, FetchCallback<T>] => {
   const [status, setStatus] = useState<FetchStatus<T>>(
-    pause ? { type: "pause" } : { type: "pending" },
+    pause ? { type: "pause" } : { type: "pending" }
   );
   const cancelTokenSourceRef = useRef<CancelTokenSource>();
 
@@ -24,49 +24,52 @@ export const useFetch = <T, D = any>({
     error,
   });
 
-  const fetchCallback = useCallback(async () => {
-    const source = axios.CancelToken.source();
-    cancelTokenSourceRef.current = source;
+  const fetchCallback: FetchCallback<T> = useCallback(
+    async (callbackRequestConfig) => {
+      const source = axios.CancelToken.source();
+      cancelTokenSourceRef.current = source;
 
-    let newStatus: FetchStatus<T> = { type: "pending" };
-    setStatus(newStatus);
+      let newStatus: FetchStatus<T> = { type: "pending" };
+      setStatus(newStatus);
 
-    try {
-      const response: AxiosResponse<T> = await axios.request<T>({
-        url,
-        withCredentials: true,
-        cancelToken: source?.token,
-        ...requestConfig,
-      });
+      try {
+        const response: AxiosResponse<T> = await axios.request<T>({
+          url,
+          withCredentials: true,
+          cancelToken: source?.token,
+          ...(callbackRequestConfig ?? requestConfig),
+        });
 
-      newStatus = { type: "success", data: response.data };
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      if (axios.isCancel(error)) {
-        // Request was canceled
-        newStatus = buildError(0, "Request cancelled");
-        return newStatus;
+        newStatus = { type: "success", data: response.data };
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        if (axios.isCancel(error)) {
+          // Request was canceled
+          newStatus = buildError(0, "Request cancelled");
+          return newStatus;
+        }
+
+        if (axiosError.response) {
+          const { status, statusText } = axiosError.response;
+          newStatus = buildError(status, statusText || "Unknown error");
+        } else if (axiosError.request) {
+          newStatus = buildError(0, "Request made but no response received");
+        } else {
+          newStatus = buildError(0, axiosError.message || "Unknown error");
+        }
       }
 
-      if (axiosError.response) {
-        const { status, statusText } = axiosError.response;
-        newStatus = buildError(status, statusText || "Unknown error");
-      } else if (axiosError.request) {
-        newStatus = buildError(0, "Request made but no response received");
-      } else {
-        newStatus = buildError(0, axiosError.message || "Unknown error");
-      }
-    }
-
-    setStatus(newStatus);
-    return newStatus;
-  }, [url, requestConfig]);
+      setStatus(newStatus);
+      return newStatus;
+    },
+    [url, requestConfig]
+  );
 
   useEffect(() => {
     if (pause) return;
 
     const fetchData = async () => {
-      await fetchCallback();
+      await fetchCallback(requestConfig);
     };
 
     fetchData();
