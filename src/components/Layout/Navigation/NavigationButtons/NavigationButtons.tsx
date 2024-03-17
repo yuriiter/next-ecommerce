e31630ffd8eae3b9b98e206705aec55bd276c328
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-03-17 03:55:40 -0700

    if not authenticated - favourites button redirects to sign-in modal. if fetching session - is disabled

--- src/components/Layout/Navigation/NavigationButtons/NavigationButtons.tsx
+++ src/components/Layout/Navigation/NavigationButtons/NavigationButtons.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { IconButton } from "@/components/Button";
 import { HeartIcon, NotificationIcon } from "@/components/svg/icons";
-import { Avatar } from "./Avatar";
 import { Badge } from "@/components/Badge";
 import { useMQ } from "@/hooks/mediaQuery/useMQ";
 import { Tooltip } from "@/components/Tooltip/Tooltip";
@@ -10,13 +9,17 @@ import { MODAL_WINDOW } from "@/types/modalWindow";
 import { Select } from "@/components/Select/Select";
 import { SelectOption } from "@/components/Select/types";
 import { AvatarMenu } from "./AvatarMenu";
+import { useAuth } from "@/auth/useAuth";
 
 export const NavigationButtons = () => {
+  const { authData } = useAuth();
   const { openWindowId, setOpenWindowId } = useModalWindow();
   const isMobile = useMQ("SM", "max");
 
   const toggleFavouritesModalWindow = () => {
-    if (openWindowId === null) setOpenWindowId(MODAL_WINDOW.FAVOURITES);
+    if (!authData.authenticated && !authData.fetching)
+      setOpenWindowId(MODAL_WINDOW.SIGN_IN);
+    else if (openWindowId === null) setOpenWindowId(MODAL_WINDOW.FAVOURITES);
     else setOpenWindowId(null);
   };
 
@@ -24,6 +27,7 @@ export const NavigationButtons = () => {
     <div className="navigation__buttons">
       <Tooltip content="Favourites" placement="center-bottom">
         <IconButton
+          disabled={!authData.authenticated && authData.fetching}
           variant="secondary"
           size={isMobile ? "sm" : "md"}
           className={`navigation__button`}
