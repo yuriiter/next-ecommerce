45a6a12f6e389c008f7c1d4588b47a23943136a8
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-09-02 04:28:50 +0200

    refactor db-related functions

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,7 +3,7 @@ import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, DB_NAME } from "@/config"
 
 const mongodbURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
 
-export const connectToMongoDB = async (): Promise<void> => {
+export const connectToDB = async (): Promise<void> => {
     console.log(`Connecting to MongoDB...`)
     try {
         await mongoose.connect(mongodbURI, {})
@@ -13,7 +13,7 @@ export const connectToMongoDB = async (): Promise<void> => {
     }
 }
 
-export const closeMongoDBConnection = async (): Promise<void> => {
+export const closeDBConnection = async (): Promise<void> => {
     try {
         await mongoose.connection.close()
         console.log("MongoDB connection closed")
