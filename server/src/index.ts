aa03fd00f207d4002242deb9fcb5d64bb2b7e60b
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-09-02 04:28:50 +0200

    refactor db-related functions

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import initRouters from "@utils/initRouters"
 import ExpressError from "@errors/ExpressError"
 import accountRouter from "@routers/account.router"
 import { FRONTEND_URL, PORT } from "@/config"
-import { closeMongoDBConnection, connectToMongoDB } from "@/db"
+import { closeDBConnection, connectToDB } from "@/db"
 import carRouter from "@routers/car.router"
 import { healthRouter } from "@routers/health.router"
 import userRouter from "@routers/user.router"
@@ -31,9 +31,9 @@ app.use(ExpressError.handleError)
 
 app.listen(PORT, () => {
     console.log(`Server is runniung on port ${PORT}`)
-    void connectToMongoDB()
+    void connectToDB()
 })
 
 app.on("close", () => {
-    void closeMongoDBConnection()
+    void closeDBConnection()
 })
