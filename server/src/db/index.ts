e2643a518962d700c86cd1f31fe7483ff428d899
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-09-03 04:18:24 +0200

    choose if we use srv in mongodb connection

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,7 +1,11 @@
 import mongoose from "mongoose"
-import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, DB_NAME } from "@/config"
+import { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME, DB_PORT } from "@/config"
 
-const mongodbURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+const SRV = true
+
+const mongodbURI = SRV
+    ? `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?authSource=admin`
+    : `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
 
 export const connectToDB = async (): Promise<void> => {
     console.log(`Connecting to MongoDB...`)
